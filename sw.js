fd26d40197aebc3548f3cba4d8df73614314d383
ilyasadaeww2005-rgb <ilyasadaeww2005@gmail.com>
2026-08-17 03:38:50 +0300

    Refresh AURA 2.0 offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aura-shell-v3';
+const CACHE = 'aura-shell-v4';
 const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-180.png'];
 
 self.addEventListener('install', event => {
